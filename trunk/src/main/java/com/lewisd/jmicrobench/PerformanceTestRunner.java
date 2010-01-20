edc4f5dade732fee111add94c418b985e30a04e6
lewisd <lewisd@0ea63342-63e7-11de-8fc2-3351cd7166e9>
2010-01-20 14:24:50 +0000

    Adding ability to configure tests to run befores/afters either before each pass, or only once per test method.

--- trunk/src/main/java/com/lewisd/jmicrobench/PerformanceTestRunner.java
+++ trunk/src/main/java/com/lewisd/jmicrobench/PerformanceTestRunner.java
@@ -14,16 +14,6 @@ import com.lewisd.test.Assert;
 
 public class PerformanceTestRunner extends BlockJUnit4ClassRunner
 {
-
-    private static final int DEFAULT_WARMUP_PASSES = Integer.parseInt(PropertiesHelper.getProperty("build.performance.warmup.passes", "0"));
-    private static final int DEFAULT_MAX_PASSES = Integer.parseInt(PropertiesHelper.getProperty("build.performance.max.passes", "100"));
-    private static final int DEFAULT_STABLE_PASSES = Integer.parseInt(PropertiesHelper.getProperty("build.performance.stable.passes", "3"));
-    private static final int DEFAULT_RUNS_TO_AVERAGE = Integer.parseInt(PropertiesHelper.getProperty("build.performance.averaged.runs", "3"));
-    private static final long DEFAULT_EXPECTED_DURATION = Long.parseLong(PropertiesHelper.getProperty("build.performance.duration", "3000"));
-    private static final long DEFAULT_WARMUP_DURATION = Long.parseLong(PropertiesHelper.getProperty("build.performance.warmup.duration", "0"));
-    private static final double DEFAULT_STABILITY_PERCENTAGE= Double.parseDouble(PropertiesHelper.getProperty("build.performance.stability.percentage", "5"));
-    private static final String DEFAULT_PROJECT_NAME = PropertiesHelper.getProperty("build.performance.project.name", "");
-
     private static final Logger LOG = Logger.getLogger(PerformanceTestRunner.class);
 
     private boolean warmedUp = false;
@@ -34,12 +24,13 @@ public class PerformanceTestRunner extends BlockJUnit4ClassRunner
     private double stabilityPercentage;
     private int runsToAverage;
     private long expectedDuration;
-    private long totalDurationNanos;
+    private boolean runBeforeAndAftersEachPass;
     private String testName;
     private String groupName;
     private String projectName;
     private final Class testClass;
 
+    private long totalDurationNanos;
     private long startTimeNanos = -1;
     private long endTimeNanos = -1;
     private int currentPass;
@@ -125,7 +116,29 @@ public class PerformanceTestRunner extends BlockJUnit4ClassRunner
     @Override
     protected Statement withAfters(FrameworkMethod method, Object target, Statement statement)
     {
-        return withRunUntilStable(method, target, super.withAfters(method, target, statement));
+        configure(method);
+        if (runBeforeAndAftersEachPass)
+        {
+            return withRunUntilStable(method, target, super.withAfters(method, target, statement));
+        }
+        else
+        {
+            return super.withBefores(method, target, super.withAfters(method, target, withRunUntilStable(method, target, statement)));
+        }
+    }
+    
+    @Override
+    protected Statement withBefores(FrameworkMethod method, Object target, Statement statement)
+    {
+        configure(method);
+        if (runBeforeAndAftersEachPass)
+        {
+            return super.withBefores(method, target, statement);
+        }
+        else
+        {
+            return statement;
+        }
     }
 
     private Statement withRunUntilStable(final FrameworkMethod method, final Object target, final Statement statement)
@@ -143,8 +156,6 @@ public class PerformanceTestRunner extends BlockJUnit4ClassRunner
 
     private void runTestUntilStable(FrameworkMethod method, Object test, Statement statement) throws Throwable
     {
-        configure(method);
-        
         resultsList = new ResultsList(runsToAverage, stablePasses, stabilityPercentage);
 
         currentPass = 0;
@@ -233,88 +244,26 @@ public class PerformanceTestRunner extends BlockJUnit4ClassRunner
         return false;
     }
     
-    private void configure(FrameworkMethod method)
+    private void configure(final FrameworkMethod method)
     {
-        configureDefaults(method);
-
-        PerformanceTest configuratinAnnotation;
-
-        configuratinAnnotation = findClassAnnotation();
-        configureFromAnnotation(configuratinAnnotation);
-
-        configuratinAnnotation = method.getAnnotation(PerformanceTest.class);
-        configureFromAnnotation(configuratinAnnotation);
-
-        if (projectName.equals(""))
-        {
-            throw new IllegalStateException("'projectName' must be configured in jmicrobench.properties on PerformanceTest annotation");
-        }
+        Configuration configuration = new Configuration(method, testClass);
+        
+        this.maxPasses = configuration.getMaxPasses();
+        this.stablePasses = configuration.getStablePasses();
+        this.stabilityPercentage = configuration.getStabilityPercentage();
+        this.runsToAverage = configuration.getRunsToAverage();
+        this.expectedDuration = configuration.getExpectedDuration();
+        this.warmupPasses = configuration.getWarmupPasses();
+        this.warmupDuration = configuration.getWarmupDuration();
+        this.groupName = configuration.getGroupName();
+        this.testName = configuration.getTestName();
+        this.projectName = configuration.getProjectName();
+        this.runBeforeAndAftersEachPass = configuration.getRunBeforeAndAftersEachPass();
         
         warmedUp = false;
         if (warmupPasses == 0 && warmupDuration == 0)
         {
             warmedUp = true;
-        }
-    }
-
-    private void configureDefaults(FrameworkMethod method)
-    {
-        this.maxPasses = DEFAULT_MAX_PASSES;
-        this.stablePasses = DEFAULT_STABLE_PASSES;
-        this.stabilityPercentage = DEFAULT_STABILITY_PERCENTAGE;
-        this.runsToAverage = DEFAULT_RUNS_TO_AVERAGE;
-        this.expectedDuration = DEFAULT_EXPECTED_DURATION;
-        this.warmupPasses = DEFAULT_WARMUP_PASSES;
-        this.warmupDuration = DEFAULT_WARMUP_DURATION;
-        this.groupName = method.getName();
-        this.testName = testClass.getSimpleName();
-        this.projectName = DEFAULT_PROJECT_NAME;
-    }
-
-    private void configureFromAnnotation(PerformanceTest configuration)
-    {
-        if (configuration != null)
-        {
-            if (configuration.warmupPasses() >= 0)
-            {
-                warmupPasses = configuration.warmupPasses();
-            }
-            if (configuration.warmupDurationMillis() >= 0)
-            {
-                warmupDuration = configuration.warmupDurationMillis();
-            }
-            if (configuration.maxPasses() >= 0)
-            {
-                maxPasses = configuration.maxPasses();
-            }
-            if (configuration.stablePasses() >= 0)
-            {
-                stablePasses = configuration.stablePasses();
-            }
-            if (configuration.stabilityPercentage() >= 0)
-            {
-                stabilityPercentage = configuration.stabilityPercentage();
-            }
-            if (configuration.runsToAverage() >= 0)
-            {
-                runsToAverage = configuration.runsToAverage();
-            }
-            if (configuration.durationMillis() >= 0)
-            {
-                expectedDuration = configuration.durationMillis();
-            }
-            if (!configuration.testName().isEmpty())
-            {
-                testName = configuration.testName();
-            }
-            if (!configuration.groupName().isEmpty())
-            {
-                groupName = configuration.groupName();
-            }
-            if (!configuration.projectName().isEmpty())
-            {
-                projectName = configuration.projectName();
-            }
         }
     }
 
